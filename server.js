d3df808532612721add80aa904d0826176605557
Gustavo Chaves <gustavojchaves1@gmail.com>
2023-09-04 19:57:12 -0300

    feat-004-crud-task

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const knex = require('knex');
 const userRoutes = require('./routes/userRoutes');
 const loginRoutes = require('./routes/loginRoutes');
 const goalRoutes = require('./routes/goalRoutes');
+const taskRoutes = require('./routes/taskRoutes');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use('/users', userRoutes);
 app.use('/login', loginRoutes);
 app.use('/goals', goalRoutes);
+app.use('/tasks', taskRoutes);
 
 const knexConfig = require('./knexfile');
 const db = knex(knexConfig.development);
