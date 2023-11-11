ae3f3849a85eaa161b417ce2d91c7d23cfe4be62
gchavess <gustavojchaves1@gmail.com>
2023-11-10 22:51:09 -0300

    fix: error sandbox

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use("/tasks", taskRoutes);
 app.use("/pomodoros", pomodoroRoutes);
 app.use("/login", loginRoutes);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Servidor iniciado na porta ${port}`);
 });
