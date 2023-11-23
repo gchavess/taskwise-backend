ecc85492939c91a670aa1db03bcee03ae9107cbf
gchavess <gustavojchaves1@gmail.com>
2023-11-23 19:35:39 -0300

    fix: remove verification goals

--- src/routes/goalRoutes.js
+++ src/routes/goalRoutes.js
@@ -1,15 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const goalController = require('../controllers/goalController');
-const userController = require('../controllers/userController');
+const goalController = require("../controllers/goalController");
+const userController = require("../controllers/userController");
 
-router.post('/', userController.verifyToken, goalController.createGoal);
-router.get('/:userId', userController.verifyToken, goalController.getAllGoals);
+router.post("/", userController.verifyToken, goalController.createGoal);
+router.get("/:userId", goalController.getAllGoals);
 router.delete(
-  '/:goalId',
+  "/:goalId",
   userController.verifyToken,
   goalController.deleteGoal
 );
-router.put('/:goalId', userController.verifyToken, goalController.updateGoal);
+router.put("/:goalId", userController.verifyToken, goalController.updateGoal);
 
 module.exports = router;
