f7397642b44aa90467338d8724e18abfb981bb3b
gchavess <gustavojchaves1@gmail.com>
2023-11-25 10:35:23 -0300

    fix: error build

--- src/routes/goalRoutes.js
+++ src/routes/goalRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const goalController = require("../controllers/goalController");
-const userController = require("../controllers/userController");
 
 router.post("/", goalController.createGoal);
 router.get("/:userId", goalController.getAllGoals);
