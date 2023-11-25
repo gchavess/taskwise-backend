fd87f14444e10055261a1f0c49936fde139cbce6
gchavess <gustavojchaves1@gmail.com>
2023-11-25 10:21:16 -0300

    fix: remove verification routes

--- src/routes/goalRoutes.js
+++ src/routes/goalRoutes.js
@@ -5,11 +5,7 @@ const userController = require("../controllers/userController");
 
 router.post("/", goalController.createGoal);
 router.get("/:userId", goalController.getAllGoals);
-router.delete(
-  "/:goalId",
-  userController.verifyToken,
-  goalController.deleteGoal
-);
-router.put("/:goalId", userController.verifyToken, goalController.updateGoal);
+router.delete("/:goalId", goalController.deleteGoal);
+router.put("/:goalId", goalController.updateGoal);
 
 module.exports = router;
