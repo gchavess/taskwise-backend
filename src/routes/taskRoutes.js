1788bf5db2027f4ec2f5cdb8d7a8d42fd30f0180
gchavess <gustavojchaves1@gmail.com>
2023-11-26 14:29:12 -0300

    fix: error build

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const taskController = require("../controllers/taskController");
-const userController = require("../controllers/userController");
 
 router.post("/", taskController.createTask);
 router.get("/", taskController.getAllTasks);
