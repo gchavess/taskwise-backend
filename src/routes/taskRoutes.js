400643d8a137859136da649a097df4b058539723
gchavess <gustavojchaves1@gmail.com>
2023-11-28 20:20:40 -0300

    fix: post by data

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const taskController = require("../controllers/taskController");
 
 router.get("/today/:userId", taskController.getAllTasksByUserIdAndToday);
-router.get("/data", taskController.getAllTasksByUserIdAndDate);
+router.post("/data", taskController.getAllTasksByUserIdAndDate);
 router.post("/", taskController.createTask);
 router.get("/", taskController.getAllTasks);
 router.delete("/:taskId", taskController.deleteTask);
